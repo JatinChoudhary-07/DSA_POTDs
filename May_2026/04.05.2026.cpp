a7cbe40697a25c5d61fe96def2ff1cc56b462d23
Jatin Choudhary <225982315+JatinChoudhary-07@users.noreply.github.com>
2026-05-04 06:41:42 +0530

    Add rotate function for 90-degree matrix rotation
    Implement rotate function to rotate a matrix by 90 degrees clockwise using transpose and reverse approach.

---
+++ May_2026/04.05.2026.cpp
@@ -0,0 +1,28 @@
+/*
+LC 48: Rotate Image
+Topic: Arrays
+Approach: Transpose + Reverse
+Time: O(n^2)
+Space: O(1)
+*/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < m; j++) {
+
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
