a7792af8c41aadf8822b378837309eaf43df0240
Jatin Choudhary <225982315+JatinChoudhary-07@users.noreply.github.com>
2026-05-23 08:38:22 +0530

    Create 23.05.2026.cpp

---
+++ May_2026/23.05.2026.cpp
@@ -0,0 +1,26 @@
+/*
+LC 1752: Check if Array Is Sorted and Rotated
+Topic: Arrays
+Approach: Count Rotation Break Points
+Time: O(n)
+Space: O(1)
+*/
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+
+        int n = nums.size();
+
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
+            }
+        }
+
+        return count <= 1;
+    }
+};
